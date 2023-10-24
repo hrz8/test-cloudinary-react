4e5b290917a9d32313b4efc676e4103638f9dc1c
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2023-10-24 20:42:53 +0700

    chore: initial commit

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+const app = document.getElementById('root')
+
+ReactDOM.createRoot(app).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
